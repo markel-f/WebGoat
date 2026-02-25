eaba9f5dc3df5f07a8f9f6fd9c252eba77512223
Markel Fernández Pinillla <m.fernandezpi.2024@alumnos.urjc.es>
2026-02-25 20:13:12 +0100

    Refactor SQL query to use prepared statements

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -55,20 +55,24 @@ public class Assignment5 extends AssignmentEndpoint {
       return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
     }
     try (var connection = dataSource.getConnection()) {
-      PreparedStatement statement =
-          connection.prepareStatement(
-              "select password from challenge_users where userid = '"
-                  + username_login
-                  + "' and password = '"
-                  + password_login
-                  + "'");
-      ResultSet resultSet = statement.executeQuery();
+        String query = "select password from challenge_users where userid = ? and password = ?";
+      try (PreparedStatement statement = connection.prepareStatement(query)) {
 
-      if (resultSet.next()) {
-        return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
-      } else {
-        return failed(this).feedback("challenge.close").build();
-      }
+    statement.setString(1, username_login);
+    statement.setString(2, password_login);
+
+    try (ResultSet resultSet = statement.executeQuery()) {
+
+        if (resultSet.next()) {
+            return success(this)
+                .feedback("challenge.solved")
+                .feedbackArgs(flags.getFlag(5))
+                .build();
+        } else {
+            return failed(this).feedback("challenge.close").build();
+        }
+    }
+}
     }
   }
 }
